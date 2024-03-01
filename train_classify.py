9d2e978f24b9d3121101afc7a063ee3f99db6e50
alvin870203 <alvin870203@gmail.com>
2024-03-01 14:05:02 +0800

    Add step-lr

--- train_classify.py
+++ train_classify.py
@@ -53,10 +53,10 @@ beta1 = 0.9  # beta1 for adamw, momentum for sgd
 beta2 = 0.999
 weight_decay = 1e-2
 grad_clip = 0.0  # clip gradients at this value, or disable if == 0.0
-decay_lr = True  # whether to decay the learning rate
+decay_lr = 'cosine'  # whether to decay the learning rate, which type of lr scheduler. False, 'cosine', 'step'
 warmup_iters = 5000  # how many steps to warm up for
-lr_decay_iters = 100000  # should be ~= max_iters
-min_lr = 1e-4  # minimum learning rate, should be ~= learning_rate/10
+lr_decay_iters = 100000  # should be ~= max_iters; this is step_size if decay_lr='step'
+min_lr = 1e-4  # minimum learning rate, should be ~= learning_rate/10; this is gamma if decay_lr='step'
 use_fused = True  # whether to use fused optimizer kernel
 # Eval related
 eval_interval = 100  # keep frequent if we'll overfit
@@ -318,18 +318,30 @@ def estimate_loss():
 
 
 # Learning rate decay scheduler (cosine with warmup)
-def get_lr(it):
-    # 1) Linear warmup for warmup_iters steps
-    if it < warmup_iters:
-        return learning_rate * it / warmup_iters
-    # 2) If it > lr_decay_iters, return min learning rate
-    if it > lr_decay_iters:
-        return min_lr
-    # 3) In between, use cosine decay down to min learning rate
-    decay_ratio = (it - warmup_iters) / (lr_decay_iters - warmup_iters)
-    assert 0 <= decay_ratio <= 1
-    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff ranges 0..1
-    return min_lr + coeff * (learning_rate - min_lr)
+if decay_lr == 'cosine':
+    def get_lr(it):
+        # 1) Linear warmup for warmup_iters steps
+        if it < warmup_iters:
+            return learning_rate * it / warmup_iters
+        # 2) If it > lr_decay_iters, return min learning rate
+        if it > lr_decay_iters:
+            return min_lr
+        # 3) In between, use cosine decay down to min learning rate
+        decay_ratio = (it - warmup_iters) / (lr_decay_iters - warmup_iters)
+        assert 0 <= decay_ratio <= 1
+        coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff ranges 0..1
+        return min_lr + coeff * (learning_rate - min_lr)
+elif decay_lr == 'step':
+    def get_lr(it):
+        # 1) Linear warmup for warmup_iters steps
+        if it < warmup_iters:
+            return learning_rate * it / warmup_iters
+        # 2) After warmup, use step decay with min_lr as gamma
+        return learning_rate * (min_lr ** ((it - warmup_iters) // lr_decay_iters))
+elif decay_lr == False:
+    pass
+else:
+    raise ValueError(f"Invalid decay_lr: {decay_lr}")
 
 
 # Logging
